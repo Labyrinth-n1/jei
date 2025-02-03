1a422207289d08d458b69d1d71fc9e0f8676c925
Labyrinth-n1 <staradisso@gmail.com>
2025-02-03 14:30:34 +0100

    Test

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,23 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  root: path.resolve(__dirname), // Racine du projet
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    process.env.NODE_ENV === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+  publicDir: 'public', // Dossier contenant index.html
+  build: {
+    outDir: 'dist', // Dossier de sortie pour le build
+  },
+});
