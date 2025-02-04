120113f0889d0abdf41d3279b114fc0d33140ca9
Labyrinth-n1 <staradisso@gmail.com>
2025-02-04 08:45:58 +0100

    popup

--- src/components/PopUp.tsx
+++ src/components/PopUp.tsx
@@ -11,7 +11,7 @@ const AutoPopup = () => {
     // Affiche le popup toutes les 2 minutes (120 000 ms)
     const interval = setInterval(() => {
       setIsOpen(true);
-    }, 20000);
+    }, 25000);
 
     return () => clearInterval(interval); // Nettoie l'intervalle
   }, []);
